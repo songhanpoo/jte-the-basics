d3a0d5fe3df9da769b1dc99659e7179bdb12d453
songhanpoo <songhanpoo@Paul-desktop.localdomain>
2022-06-15 14:25:38 +0700

    debug

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,9 +1,4 @@
-jte{
-    // permissive_initialization = true
-    // reverse_library_resolution = true
-    pipeline_template = "pipeline_config.groovy"
-}
 
 @merge libraries{
-    sonarqube
+  sonarqube
 }
