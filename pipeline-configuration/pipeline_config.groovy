5d8cea18d76e1e066ab22f873061ca100bac9b12
songhanpoo <songhanpoo@Paul-desktop.localdomain>
2022-06-15 16:41:49 +0700

    debug

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,10 +1,3 @@
-jte{
-    allow_scm_jenkinsfile = false
-    permissive_initialization = true
-    // pipeline_template = "pipeline_config.groovy"
-    reverse_library_resolution = true
-}
-
 libraries{
     sonarqube
 }
